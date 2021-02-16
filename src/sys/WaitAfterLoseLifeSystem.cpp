5a6f5be97a26d61005966cccfc4d72ad8ffdbd52
Daninimas <dbg29@alu.ua.es>
2021-02-16 09:28:56 +0100

    fixed wait after lose health

--- src/sys/WaitAfterLoseLifeSystem.cpp
+++ src/sys/WaitAfterLoseLifeSystem.cpp
@@ -11,8 +11,8 @@ void WaitAfterLoseLifeSystem::update(GameEngine& gameContext) const {
 	WorldElementsData::waitLoseLifeTimeCounter += gameContext.getDeltaTime();
 
 	if (WorldElementsData::waitLoseLifeTimeCounter > WorldElementsData::waitLoseLifeTime) {
-		gameContext.popGameState(); // From WAIT_AFTER_LOSE_LIFE to PLAYING again
-
 		WorldElementsData::waitLoseLifeTimeCounter = 0.f; // Reset the counter again
+
+		gameContext.popGameState(); // From WAIT_AFTER_LOSE_LIFE to PLAYING again
 	}
 }
